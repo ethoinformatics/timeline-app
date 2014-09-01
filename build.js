2b1bbd2b39400d1a1666340a56067111c9f65bad
Mike Chevett <mchevett@gmail.com>
2014-09-01 13:47:10 -0400

    no brfs

--- build.js
+++ build.js
@@ -2,13 +2,11 @@ var fs = require('fs'),
 	mkdirp = require('mkdirp'),
 	browserify = require('browserify'),
 	lessify = require('node-lessify'),
-	brfs = require('brfs'),
 	vashify = require('vashify');
 
 mkdirp.sync(__dirname + '/public/bundles');
 
 var b = browserify();
-b.transform(brfs); // inlines file contents.  used for reactive template strings
 b.transform(lessify); // builds and applies css from .less files
 b.transform(vashify); // compiles .vash files in to js template functions
 
