60fe9e06f3efb94a1b62b56457fb462f4fe10e39
Mike Chevett <mchevett@gmail.com>
2014-09-01 13:47:56 -0400

    very rough timeline "working"

--- src/views/timeline/index.js
+++ src/views/timeline/index.js
@@ -1,5 +1,9 @@
+require('./index.less');
+
 var $ = window.$,
-	cubism = require('cubism'),
+	_ = require('lodash'),
+	d3 = require('d3'),
+	d3Timeline = require('./d3-timeline')(d3),
 	storage = require('jocal'),
 	template = require('./index.vash');
 
@@ -9,8 +13,39 @@ function Timeline(){
 
 	self.hide = self.$element.hide.bind(self.$element);
 	self.show = function(){
-		self.$element.find('pre').text(JSON.stringify(storage('activities'), '\t', 4));
 		self.$element.show();
+		self.$element.find('#timeline-container').empty();
+
+		var activities = storage('activities') || [];
+		activities = _.cloneDeep(activities);
+
+		activities = activities.map(function(activity, i){
+			activity.starting_time = new Date(activity.starting_time).valueOf();
+			activity.ending_time = Date.now();
+
+
+			return {
+				label: activity.type + ' ' + i,
+				times: [activity],
+			};
+		});
+	// var activities = [
+        // {label: "person a", times: [{"starting_time": 1355752800000, "ending_time": 1355759900000}, {"starting_time": 1355767900000, "ending_time": 1355774400000}]},
+        // {label: "person b", times: [{"starting_time": 1355759910000, "ending_time": 1355761900000}, ]},
+        // {label: "person c", times: [{"starting_time": 1355761910000, "ending_time": 1355763910000}]},
+      // ];
+
+		var chart = d3.timeline()
+			.stack()
+			.margin({left:70, right:30, top:0, bottom:0});
+
+
+		d3.select('#timeline-container')
+			.append('svg')
+			.attr('width', window.innerWidth)
+			.datum(activities)
+			.call(chart);
+			
 	};
 }
 
