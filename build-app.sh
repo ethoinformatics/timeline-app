0d9a45e2b64a1220caa48d9e9dc157355a14e600
Mike Chevett <mchevett@gmail.com>
2014-09-09 08:28:16 -0400

    hookup build and publish scripts for hockey app

---
+++ build-app.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$DIR"
+npm install;
+node build.js
+
+
+cd "$DIR"/ionic
+cordova platform rm android
+cordova platform add android
+cordova build android
+
+echo 'great job.'
