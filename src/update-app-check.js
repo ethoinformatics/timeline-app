04ea837eb638e1291056d6d91e9ae948522f128f
Mike Chevett <mchevett@gmail.com>
2014-09-14 13:18:42 -0400

    don't need this hockeyapp message with every load

--- src/update-app-check.js
+++ src/update-app-check.js
@@ -1,9 +1,7 @@
 /* global HockeyApp */
 
 module.exports = function updateCheck(){
-	if (!HockeyApp){
-		return console.log('skipping hockeyapp.  this should be desktop browsers only.');
-	}
+	if (typeof HockeyApp === 'undefined') return;
 
 	HockeyApp.init(
 		[ 'fce2c0e86b0cd9989fb9d6db7688cba3', true, true ],
