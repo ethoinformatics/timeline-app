df39749a15af89a69c526494b4ac07de4f691fdf
Mike Chevett <mchevett@gmail.com>
2014-10-07 08:39:44 -0400

    load sample data maybe?

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 var $ = require('jquery'),
 	ActivityPage = require('./views/activity-page/'),
 	updateAppCheck = require('./update-app-check'),
+	storage = require('jocal'),
+	sampleData = require('./sample-data.json'),
+	_ = require('lodash'),
 	ready = require('dom-ready');
 
 var sideMenu = require('./side-menu.js');
@@ -14,5 +17,10 @@ ready(function appLoad(){
 	//activityPage.show();
 
 	updateAppCheck();
+
+
+	if (window.confirm('Load the sample data?')){
+		storage('activities', _.first(sampleData, 15));
+	}
 });
 
