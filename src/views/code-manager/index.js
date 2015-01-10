16a18eb683a8aea0b6a4a464b6a73adf0ed975cc
Mike Chevett <mchevett@gmail.com>
2015-01-09 23:21:50 -0500

    show the codes

--- src/views/code-manager/index.js
+++ src/views/code-manager/index.js
@@ -4,6 +4,7 @@ var tmpl = require('./index.vash'),
 	_ = require('lodash'),
 	$ = require('jquery'),
 	Modal = require('modal'),
+	listItemTemplate = require('./list-item-template.vash'),
 	app = require('app');
 
 
@@ -33,7 +34,7 @@ function CodeManager(){
 			createNewDialog.on('created', function(data){
 				createNewDialog.hide();
 				self.show();
-				_refresh();
+				_refresh(data);
 			});
 
 			createNewDialog.on('closed', function(){
@@ -48,7 +49,10 @@ function CodeManager(){
 		hideOkay: true,
 	});
 
-	self.show = function(){ modal.show(); };
+	self.show = function(){ 
+		modal.show();
+		_load();
+	};
 
 	function _getCurrentDomain(){
 		if (!$domainSelect) return null;
@@ -57,16 +61,47 @@ function CodeManager(){
 		return _.find(codeDomains, function(d){ return d.name == selectedDomainName; });
 	}
 
-	function _refresh(){
+	function _refresh(newEntity){
 		var currentDomain = _getCurrentDomain();
-		var entityManager = currentDomain.getService('entity-manager');
-		entityManager.getAll()
+		var entityManager = currentDomain.getService('entity-manager'),
+			descManager = currentDomain.getService('description-manager');
+
+		entityManager.save(newEntity)
+			.then(function(){
+				return entityManager.getAll();
+			})
 			.then(function(entities){
-				debugger
+				var $list = $element.find('.js-list');
+				entities.forEach(function(entity){
+					var el = listItemTemplate({
+						id: entity._id,
+						label: descManager.getShortDescription(entity),
+					});
+					$list.append(el);
+					
+				});
 			})
 			.done();
+	}
 
+	function _load(){
+		var currentDomain = _getCurrentDomain();
+		var entityManager = currentDomain.getService('entity-manager'),
+			descManager = currentDomain.getService('description-manager');
 
+		return entityManager.getAll()
+			.then(function(entities){
+				var $list = $element.find('.js-list');
+				entities.forEach(function(entity){
+					var el = listItemTemplate({
+						id: entity._id,
+						label: descManager.getShortDescription(entity),
+					});
+					$list.append(el);
+					
+				});
+			})
+			.done();
 	}
 }
 
