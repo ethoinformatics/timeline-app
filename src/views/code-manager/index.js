c6bb0b7b9787142430a820256ee3588bb7a64624
Mike Chevett <mchevett@gmail.com>
2015-01-10 20:08:02 -0500

    fix coded fields

--- src/views/code-manager/index.js
+++ src/views/code-manager/index.js
@@ -1,45 +1,76 @@
 require('./index.less');
 var tmpl = require('./index.vash'),
 	CreateNewDialog = require('create-new-dialog'),
+	Scroll = require('iscroll'),
 	_ = require('lodash'),
 	$ = require('jquery'),
 	Modal = require('modal'),
 	listItemTemplate = require('./list-item-template.vash'),
 	app = require('app');
 
-
-
 function CodeManager(){
 	var self = this,
 		codeDomains = app.getDomains('code-domain');
-
-	console.dir('--- code domains ---');
-	console.dir(codeDomains);
 
 	var $element = $(tmpl({
 		domains: codeDomains,
 	}));
 
-	var $domainSelect = $element.find('.js-select-domain');
+	var $domainSelect = $element.find('.js-select-domain'),
+		$codeList = $element.find('.js-codes');
+
+	$domainSelect.on('change', function(){
+		_load();
+	});
+
+	// var scroll = new Scroll($element.find('.scroll-wrapper')[0], {
+	// 		mouseWheel: true,
+	// 		scrollbars: true,
+	// 	});
+	$codeList.on('click', '.js-delete', function(){
+		if (!window.confirm('Are you sure?')) return;
+
+		var $this = $(this),
+			$item = $this.closest('.item'),
+			_id = $item.data('_id'),
+			_rev = $item.data('_rev');
+
+		self.entityManager.remove({_id: _id, _rev: _rev})
+			.then(function(){
+				$item.fadeOut(function(){
+					$item.remove();
+				});
+			})
+			.catch(function(err){
+				console.error(err);
+			});
+	});
+
 	$element.find('.js-btn-add')
 		.click(function(ev){
 			ev.preventDefault();
 
-			var currentDomain = _getCurrentDomain();
-
 			var createNewDialog = new CreateNewDialog({
-					domain: currentDomain,
+					domain: self.currentDomain,
 				});
 
 			createNewDialog.on('created', function(data){
 				createNewDialog.hide();
-				self.show();
-				_refresh(data);
+				modal.show();
+
+				self.entityManager.save(data)
+					.then(function(){
+						_load();
+					})
+					.catch(function(err){
+						console.error(err);
+					});
 			});
 
 			createNewDialog.on('closed', function(){
 				self.show();
 			});
+
 			createNewDialog.show();
 		});
 
@@ -54,52 +85,58 @@ function CodeManager(){
 		_load();
 	};
 
-	function _getCurrentDomain(){
-		if (!$domainSelect) return null;
+	Object.defineProperty(self, 'currentDomain', {
+			get: function(){ 
+				if (!$domainSelect) return null;
 
-		var selectedDomainName = $domainSelect.val();
-		return _.find(codeDomains, function(d){ return d.name == selectedDomainName; });
-	}
+				var selectedDomainName = $domainSelect.val();
+				return _.find(codeDomains, function(d){ return d.name == selectedDomainName; });
+			},
+		});
 
-	function _refresh(newEntity){
-		var currentDomain = _getCurrentDomain();
-		var entityManager = currentDomain.getService('entity-manager'),
-			descManager = currentDomain.getService('description-manager');
+	Object.defineProperty(self, 'entityManager', {
+			get: function(){ 
+				if (!self.currentDomain) return null;
 
-		entityManager.save(newEntity)
-			.then(function(){
-				return entityManager.getAll();
-			})
-			.then(function(entities){
-				var $list = $element.find('.js-list');
-				entities.forEach(function(entity){
-					var el = listItemTemplate({
-						id: entity._id,
-						label: descManager.getShortDescription(entity),
-					});
-					$list.append(el);
-					
-				});
-			})
-			.done();
+				return self.currentDomain.getService('entity-manager');
+			},
+		});
+
+	Object.defineProperty(self, 'descriptionManager', {
+			get: function(){ 
+				if (!self.currentDomain) return null;
+
+				return self.currentDomain.getService('description-manager');
+			},
+		});
+
+	function _createListItemElement(entity){
+		return listItemTemplate({
+			_id: entity._id,
+			_rev: entity._rev,
+			label: self.descriptionManager.getShortDescription(entity) || 'fuck'
+		});
 	}
 
 	function _load(){
-		var currentDomain = _getCurrentDomain();
-		var entityManager = currentDomain.getService('entity-manager'),
-			descManager = currentDomain.getService('description-manager');
+		$codeList.empty()
+			.append('Loading...');
 
-		return entityManager.getAll()
+		return self.entityManager.getAll()
 			.then(function(entities){
-				var $list = $element.find('.js-list');
-				entities.forEach(function(entity){
-					var el = listItemTemplate({
-						id: entity._id,
-						label: descManager.getShortDescription(entity),
+				$codeList.empty();
+				if (_.isEmpty(entities)){
+					$codeList.append('There are no ' + self.currentDomain.label.toLowerCase() + ' codes.');
+				}
+				entities
+					.map(_createListItemElement)
+					.forEach(function(el){
+						$codeList.append(el);
 					});
-					$list.append(el);
-					
-				});
+
+				setTimeout(function(){
+//					scroll.refresh();
+				},0);
 			})
 			.done();
 	}
