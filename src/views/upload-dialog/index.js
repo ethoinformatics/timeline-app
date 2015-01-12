1a2f19330729509d99c47fdfe38869a10f27da9c
Mike Chevett <mchevett@gmail.com>
2015-01-11 21:59:53 -0500

    fix the upload dialog

--- src/views/upload-dialog/index.js
+++ src/views/upload-dialog/index.js
@@ -1,5 +1,11 @@
-var template = require('./index.vash'),
+require('./index.less');
+
+var PouchDb = require('pouchdb'),
+	template = require('./index.vash'),
+	_ = require('lodash'),
 	$ = require('jquery'),
+	storage = require('jocal'),
+	passwordStore = storage.bind(null, 'etho-pw'),
 	app = require('app'),
 	Modal = require('modal');
 
@@ -8,7 +14,12 @@ function UploadDialog(){
 		$element = $(template({
 				url: app.setting('couch-base-url'),
 				username: app.setting('couch-username'),
+				password: passwordStore(),
 			})),
+		$status = $element.find('.js-status'),
+		$userName = $element.find('.js-username'),
+		$password = $element.find('.js-password'),
+		$url = $element.find('.js-url'),
 		modal = new Modal({
 				title:'Upload', 
 				$content:$element, 
@@ -19,21 +30,66 @@ function UploadDialog(){
 		modal.show();
 	};
 
-	$element.find('.js-upload').click(function(){
-		var $userName = $element.find('.js-username'),
-			$password = $element.find('.js-password'),
-			$url = $element.find('.js-url');
+	function _showMessage(selector, text){
+		$status.find('.message')
+			.hide()
+			.filter(selector)
+			.show()
+			.find('span')
+			.text(text);
+	}
 
+	var _showWorking = _.partial(_showMessage, '.js-working'),
+		_showSuccess = _.partial(_showMessage, '.js-success'),
+		_showError = _.partial(_showMessage, '.js-failure');
+
+	function _getUrl(){
 		var url = $url.val();
 		url = url.replace('//', '//'+$userName.val() + ':' + $password.val() + '@');
-		console.dir(url);
 
-		db.upload(url)
-			.then(function(){
-				window.alert('success');
-				modal.hide();
+		return url;
+	}
+
+	$element.find('.js-upload').click(function(){
+		var db = new PouchDb('hello'),
+			password = $password.val();
+		_showWorking('Uploading...');
+
+		db.replicate.to(_getUrl(), {live:false})
+			.on('complete', function(info){
+				passwordStore(password);
+				if (info.docs_written === 0){
+					_showSuccess('No new data to upload.');
+				} else if (info.docs_written === 1){
+					_showSuccess('Upload complete.  Sent 1 change.');
+				} else {
+					_showSuccess('Upload complete.  Sent ' + info.docs_written + ' changes.');
+				}
+			})
+			.on('error', function(err){
+				_showError('Upload errror: ' + err.message);
 			});
+	});
 
+	$element.find('.js-download').click(function(){
+		var db = new PouchDb('hello'),
+			password = $password.val();
+
+		_showWorking('Downloading...');
+		db.replicate.from(_getUrl(), {live: false})
+			.on('complete', function(info){
+				passwordStore(password);
+				if (info.docs_written === 0){
+					_showSuccess('Already up to date.');
+				} else if (info.docs_written === 1){
+					_showSuccess('Download complete.  Received 1 change.');
+				} else {
+					_showSuccess('Upload complete.  Received ' + info.docs_written + ' changes.');
+				}
+			})
+			.on('error', function(err){
+				_showError('Download errror: ' + err.message);
+			});
 	});
 }
 
