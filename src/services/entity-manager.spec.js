6e2782928be532f1699e26c4500b89c365e3cc5d
Mike Chevett <mchevett@gmail.com>
2015-09-14 14:47:07 -0400

    fix tests

--- src/services/entity-manager.spec.js
+++ src/services/entity-manager.spec.js
@@ -5,15 +5,17 @@ var expect = require('chai').expect;
 describe('entity manager', function(){
 	it('should generate an id property for a new child', function(){
 		// todo: get registrar out of here
-		var p = registrar.createDomain({name: 'parent_domain'});
-		var c = registrar.createDomain({name: 'child_domain'});
+		var PARENT_DOMAIN_NAME = Date.now()+'parent';
+		var CHILD_DOMAIN_NAME = Date.now()+'child';
+		var p = registrar.createDomain({name: PARENT_DOMAIN_NAME});
+		var c = registrar.createDomain({name: CHILD_DOMAIN_NAME});
 		p.register('myList', c);
 
 		var parentEntity = {myList:[], };
 		var childEntity = {message: 'hey there, i am a child entity.'};
 
 		var registry = registrar.getRegistry();
-		var childDomain = registry.getDomain('child_domain');
+		var childDomain = registry.getDomain(CHILD_DOMAIN_NAME);
 		var childEntityManager = childDomain.getService('entity-manager');
 
 		childEntityManager.addToParent(parentEntity, childEntity);
